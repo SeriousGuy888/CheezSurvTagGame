1846e459f158f57f64e2f34b63c9635aa38df97a
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-01-10 17:25:44 -0500

    added command for checking tag stats

--- src/main/java/io/github/seriousguy888/cheezsurvtaggame/commands/StatsCommand.java
+++ src/main/java/io/github/seriousguy888/cheezsurvtaggame/commands/StatsCommand.java
@@ -2,6 +2,9 @@ package io.github.seriousguy888.cheezsurvtaggame.commands;
 
 import io.github.seriousguy888.cheezsurvtaggame.CheezSurvTagGame;
 import io.github.seriousguy888.cheezsurvtaggame.TagStatsProfile;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,22 +20,39 @@ public class StatsCommand implements CommandExecutor {
   }
 
   @Override
+  @SuppressWarnings("deprecation")
   public boolean onCommand(@Nonnull CommandSender sender,
                            @Nonnull Command command,
                            @Nonnull String label,
                            @Nonnull String[] args) {
 
-//    if(args[0] == null)
-//      return false;
-
-    if(!(sender instanceof Player player)) {
-      sender.sendMessage("no");
-      return false;
+    OfflinePlayer lookupPlayer;
+    if(args.length == 0) { // if no args supplied, default to the sender if the sender is not the console
+      if(sender instanceof Player)
+        lookupPlayer = (OfflinePlayer) sender;
+      else {
+        sender.sendMessage(ChatColor.RED + "Provide a player to look up the stats of.");
+        return true;
+      }
+    } else {
+      // getting an offline player by their username is deprecated, but apparently it is not
+      // going to be removed any time soon. at least, I sure hope it doesn't!
+      lookupPlayer = Bukkit.getOfflinePlayer(args[0]);
     }
 
-    TagStatsProfile statsProfile = plugin.game.getTagStats(player);
-    player.sendMessage("tags given" + statsProfile.getTagsGiven());
-    player.sendMessage("tags taken" + statsProfile.getTagsTaken());
+    if(!lookupPlayer.hasPlayedBefore()) {
+      sender.sendMessage(ChatColor.RED + "This player has not joined the server before.");
+      return true;
+    }
+
+
+    TagStatsProfile statsProfile = plugin.game.getTagStats(lookupPlayer);
+    sender.sendMessage("\n" + ChatColor.BLUE + ChatColor.BOLD +
+        lookupPlayer.getName() + "'s CheezSurv Tag Stats" +
+        ChatColor.DARK_AQUA + "\nTimes Tagged by Other Players: " + ChatColor.AQUA +
+        statsProfile.getTagsGiven() +
+        ChatColor.DARK_AQUA + "\nTimes Having Tagged Another Player: " + ChatColor.AQUA +
+        statsProfile.getTagsTaken());
     return true;
   }
 }
