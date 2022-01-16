9727c0487a60bf77dc8fc0dd696d44c7bd719551
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-01-16 13:43:45 -0500

    save the tag game data and stats every 15 minutes just in case the server crashes and does not shut down properly

--- src/main/java/io/github/seriousguy888/cheezsurvtaggame/CheezSurvTagGame.java
+++ src/main/java/io/github/seriousguy888/cheezsurvtaggame/CheezSurvTagGame.java
@@ -2,6 +2,7 @@ package io.github.seriousguy888.cheezsurvtaggame;
 
 import io.github.seriousguy888.cheezsurvtaggame.commands.ItCommand;
 import io.github.seriousguy888.cheezsurvtaggame.commands.StatsCommand;
+import io.github.seriousguy888.cheezsurvtaggame.runnables.SaveGameData;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -17,11 +18,13 @@ public final class CheezSurvTagGame extends JavaPlugin {
     this.getServer().getPluginManager().registerEvents(new TagEvents(this), this);
     Objects.requireNonNull(this.getCommand("it")).setExecutor(new ItCommand(this));
     Objects.requireNonNull(this.getCommand("tagstats")).setExecutor(new StatsCommand(this));
+
+    // save the tag game data to data.yml every 15 minutes
+    new SaveGameData(this).runTaskTimer(this, 0, 15 * 60 * 20);
   }
 
   @Override
   public void onDisable() {
-    game.saveState();
-    game.saveTagStats();
+    new SaveGameData(this).run();
   }
 }
---
+++ src/main/java/io/github/seriousguy888/cheezsurvtaggame/runnables/SaveGameData.java
@@ -0,0 +1,17 @@
+package io.github.seriousguy888.cheezsurvtaggame.runnables;
+
+import io.github.seriousguy888.cheezsurvtaggame.CheezSurvTagGame;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class SaveGameData extends BukkitRunnable {
+  private final CheezSurvTagGame plugin;
+  public SaveGameData(CheezSurvTagGame plugin) {
+    this.plugin = plugin;
+  }
+
+  @Override
+  public void run() {
+    plugin.game.saveState();
+    plugin.game.saveTagStats();
+  }
+}
