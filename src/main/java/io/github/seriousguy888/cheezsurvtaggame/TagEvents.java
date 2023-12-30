79ff9bd2879d7db9c93c2345ab87217013636f35
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-12-29 22:16:20 -0500

    allow ranged attacks when playing tag

--- src/main/java/io/github/seriousguy888/cheezsurvtaggame/TagEvents.java
+++ src/main/java/io/github/seriousguy888/cheezsurvtaggame/TagEvents.java
@@ -5,41 +5,64 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 public class TagEvents implements Listener {
   private final CheezSurvTagGame plugin;
+
   public TagEvents(CheezSurvTagGame plugin) {
     this.plugin = plugin;
   }
 
   @EventHandler
   public void onTag(EntityDamageByEntityEvent event) {
-    if(!(event.getEntity() instanceof Player victim))
-      return;
-    if(!(event.getDamager() instanceof Player damager))
+    if (!(event.getEntity() instanceof Player victim))
       return;
 
+    Player damager;
+
+    Entity damagingEntity = event.getDamager();
+    if (damagingEntity instanceof Player) {
+      // If the direct damaging entity was a player, designate that player as the damager.
+      damager = (Player) damagingEntity;
+    } else if (damagingEntity instanceof Projectile) {
+      // If the direct damaging entity was a projectile, test if there was a player that shot
+      // said projectile. If so, that player is the damager, and we will continue as if ranged
+      // attack was any other attack.
+
+      ProjectileSource shooter = ((Projectile) damagingEntity).getShooter();
+      if (shooter instanceof Player) {
+        damager = (Player) shooter;
+      } else {
+        return;
+      }
+    } else {
+      return;
+    }
+
     OfflinePlayer it = plugin.game.getIt();
-    if(it == null)
+    if (it == null)
       return;
 
     // check equality of uuids because the damager is a Player and the player who is it is an OfflinePlayer
-    if(!damager.getUniqueId().equals(it.getUniqueId()))
+    if (!damager.getUniqueId().equals(it.getUniqueId()))
       return;
-    if(!Bukkit.getOnlinePlayers().contains(victim))
+    if (!Bukkit.getOnlinePlayers().contains(victim))
       return; // crude test to try to prevent tagging npcs
 
 
     long tagbackCooldownMs = 3000; // how long players must wait before tagging back the player who tagged them
     long msSinceLastTag = plugin.game.getMsSinceLastTag();
     double cooldownRemaining = (double) (tagbackCooldownMs - msSinceLastTag) / 1000;
-    if(victim == plugin.game.getPreviousIt() && msSinceLastTag < tagbackCooldownMs) {
+    if (victim == plugin.game.getPreviousIt() && msSinceLastTag < tagbackCooldownMs) {
       damager.sendMessage(ChatColor.GRAY +
           "Wait " + cooldownRemaining + " seconds to tag back the player who tagged you.");
       return;
@@ -69,7 +92,7 @@ public class TagEvents implements Listener {
     plugin.game.loadTagStats(player);
 
     OfflinePlayer it = plugin.game.getIt();
-    if(it != null)
+    if (it != null)
       player.sendMessage(ChatColor.GRAY + it.getName() + " is currently It.");
   }
 
