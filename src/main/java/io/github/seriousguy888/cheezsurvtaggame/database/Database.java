6d2f51887bad65adb137c535d33b4471241caf06
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2024-01-03 15:58:33 -0500

    switched to using sqlite since it's provided by spigot

--- src/main/java/io/github/seriousguy888/cheezsurvtaggame/database/Database.java
+++ src/main/java/io/github/seriousguy888/cheezsurvtaggame/database/Database.java
@@ -10,34 +10,41 @@ public class Database {
 
     private final CheezSurvTagGame plugin;
     private final String dbName;
+    private Connection connection;
 
     public Database(CheezSurvTagGame plugin, String dbName) {
         this.plugin = plugin;
         this.dbName = dbName;
+        this.connection = null;
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public Connection getConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                return connection;
+            }
+        } catch (SQLException e) {
+            // If here, do nothing and continue below to try to create a new connection.
+        }
 
         File folder = plugin.getDataFolder();
         if (!folder.exists()) {
             folder.mkdirs();
         }
 
+        File dbFile = new File(folder, "data" + File.separator + dbName + ".db");
+        String url = "jdbc:sqlite:" + dbFile;
 
-        String url = "jdbc:h2:file:"
-                + folder.getAbsolutePath() + File.separator + "data" + File.separator + dbName;
-
-        Connection connection = null;
         try {
-            Class.forName("org.h2.Driver");
+            Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection(url);
+            return connection;
         } catch (SQLException | ClassNotFoundException e) {
             plugin.getLogger().log(Level.SEVERE, "Failed to connect to database.", e);
         }
 
-        return connection;
-
+        return null;
     }
 
     public void init() {
